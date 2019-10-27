0256afd6555ae54320389fd293a815ce5c5485b3
thepsych0 <edserobian@yandex.ru>
2019-10-27 13:15:31 +0300

    filtering events by tournament and game

--- Sources/App/Controllers/EventsController.swift
+++ Sources/App/Controllers/EventsController.swift
@@ -3,6 +3,9 @@ import FluentPostgreSQL
 
 final class EventsController {
     func index(_ req: Request, tournamentID: Int, gameID: String) throws -> Future<[Event]> {
-        return Event.query(on: req).all()
+        return Event.query(on: req)
+            .filter(\Event.tournamentID == tournamentID)
+            .filter(\Event.gameID == gameID)
+            .all()
     }
 }
