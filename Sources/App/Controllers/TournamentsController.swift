58e56c3d25c4a808b35fd33c1f2144329e2d4a88
thepsych0 <thepsych0@yandex-team.ru>
2019-11-08 01:50:09 +0300

    static tournaments

--- Sources/App/Controllers/TournamentsController.swift
+++ Sources/App/Controllers/TournamentsController.swift
@@ -2,7 +2,11 @@ import Vapor
 import FluentPostgreSQL
 
 final class TournamentsController {
-    func index(_ req: Request) throws -> Future<[Tournament]> {
-        return Tournament.query(on: req).all()
+    func index(_ req: Request) throws -> [Tournament] {
+        return ServerModels.tournaments
     }
+}
+
+final class ServerModels {
+    static let tournaments = [Tournament(id: 1, title: "MGIMO-International", date: 1573285500, games: ["chgk","brainNoF","ek","si","other"])]
 }
