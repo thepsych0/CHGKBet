68c621b4cc410160e2529b0c5036447924219e91
thepsych0 <thepsych0@yandex-team.ru>
2019-12-11 13:57:09 +0300

    added logo folder

--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -5,8 +5,9 @@ import Crypto
 /// Register your application's routes here.
 public func routes(_ router: Router) throws {
     // Basic "It works" example
-    router.get { req in
-        return "It works!"
+    router.get { req -> String in
+        let some = try! Environment.detect()
+        return "\(some.name), \(NIOServerConfig.default())"
     }
 
     //let websiteController = WebsiteController()
