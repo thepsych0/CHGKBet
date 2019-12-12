c210d4c729d89249fd25c544833d1bcd7c4acd0f
thepsych0 <edserobian@yandex.ru>
2019-12-13 00:01:44 +0300

    removed handbrakes

--- Sources/App/Controllers/EventsController.swift
+++ Sources/App/Controllers/EventsController.swift
@@ -3,13 +3,9 @@ import FluentPostgreSQL
 
 final class EventsController {
     func index(_ req: Request, tournamentID: Int, gameID: String) throws -> Future<[Event]> {
-        //handbrake
-        let query = Event.query(on: req)
+        return Event.query(on: req)
             .filter(\Event.tournamentID == tournamentID)
             .filter(\Event.gameID == gameID)
             .all()
-        return query.map { events -> [Event] in
-            return events.filter { $0.tournamentID == 1 }
-        }
     }
 }
